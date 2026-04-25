7a2324c5f6b734c44712686836bea5abc7f231b6
laflamme <adityaunalepic@gmail.com>
2026-04-25 15:25:48 +0530

    Send Question

--- CF_2008_E.cpp
+++ CF_2008_E.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int MOD = 998244353;
 
 int findmin(vector<int> arr){
+    // for(int i = 0;i < 26;i +=1){
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<endl;
     int maxFreq = 0;
     int maxChar = 0;
     for(int i = 0;i < 26;i +=1){
@@ -16,22 +20,23 @@ int findmin(vector<int> arr){
             res +=arr[i]; 
         }
     }
-    cout<<res<<endl;
+    // cout<<"res : "<<res<<endl;
     return res;
 }
 
-void printArray(vector<int> arr){
-    for(int i:arr){
-        cout<<i<<" ";
+vector<int> addArray(vector<int> arr1, vector<int> arr2){
+    vector<int> res(26,0);
+    for(int i = 0;i < 26;i +=1){
+        res[i] = arr1[i] + arr2[i];
     }
-    cout<<endl;
+    return res;
 }
 
 int main()
 {
     int T;
     cin>>T;
-    vector<int> results;
+    // vector<int> results;
     while(T){
         int n;
         string s;
@@ -45,20 +50,21 @@ int main()
             // vector<int> postEven(26,0);
             int res = INT_MAX;
             for(int i = n - 1;i > 0;i--){
-
+                postOdd[i-1] = postOdd[i];
+                postEven[i-1] = postEven[i];
                 if(i%2){
                     postOdd[i - 1][s[i] - 'a'] +=1;
                 }
                 else{
                     postEven[i - 1][s[i]-'a'] +=1;
                 }
-                cout<<"Post "<<(i - 1)<<" array for odd Places :"<<endl;
-                printArray(postOdd[i - 1]);
-                cout<<"Post "<<(i - 1)<<" array for even Places :"<<endl;
-                printArray(postEven[i - 1]);
             }
             for(int i = 0;i < n;i +=1){
-                res = min(res,findmin(preOdd)+findmin(preEven)+findmin(postOdd[i])+findmin(postEven[i]) + 1);
+                // cout<<i<<" : "<<findmin(preOdd)+findmin(preEven)+findmin(postOdd[i])+findmin(postEven[i]) + 1<<endl;
+                // cout<<i<<" : "<<findmin(preOdd)<<" "<<findmin(preEven)<<" "<<findmin(postOdd[i])<<" "<<findmin(postEven[i])<<endl;
+                // cout<<i<<" : "<<printArray(preOdd)<<printArray(preEven)<<printArray(postOdd[i])<<printArray(postEven[i])<<endl;
+                // cout<<i<<" : ";
+                res = min(res,findmin(addArray(preOdd,postEven[i]))+findmin(addArray(preEven, postOdd[i])) + 1);
                 if(i%2){
                     preOdd[s[i] - 'a'] +=1;
                 }
@@ -67,7 +73,7 @@ int main()
                 }
             }
             
-            results.push_back(res);
+            // results.push_back(res);
             cout<<res<<endl;
         }
         else{
@@ -81,14 +87,14 @@ int main()
                     even[s[i]-'a'] +=1;
                 }
             }
-            results.push_back(findmin(odd) + findmin(even));
+            // results.push_back(findmin(odd) + findmin(even));
             cout<<findmin(odd) + findmin(even)<<endl;
         }
         T--;
     }
-    cout<<"Results :"<<endl;
-    for(int i:results){
-        cout<<i<<endl;
-    }
+    // cout<<"Results :"<<endl;
+    // for(int i:results){
+    //     cout<<i<<endl;
+    // }
     return 0;
 }
